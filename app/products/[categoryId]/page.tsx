fb549f2741aebd15bde2827b063a397ac4d77c4e
Ramy <ramy.magdy@rockettravelsystem.com>
2024-08-19 17:11:59 +0300

    shadow box products

--- app/products/[categoryId]/page.tsx
+++ app/products/[categoryId]/page.tsx
@@ -22,36 +22,40 @@ const Page = ({ params }: { params: { categoryId: string } }) => {
       {params.categoryId}
       <PageBanner adddress={`${products?.categoryName} Products`} />
 
-      <div className="container">
-        <div className="homeSection">
-          <div className="row mt-5 gap-5">
-            {products?.products.map((category, i) => (
-              <div key={i} className="col-md-12">
-                <div
-                  className={`d-flex align-items-center gap-4 flex-column flex-md-row ${
-                    i % 2 != 0 && "flex-md-row-reverse"
-                  }`}
-                >
-                  <div className="w-50">
-                    <h2 className="fw-bold">{category.name}</h2>
-                    <p>{category.desc}</p>
-                  </div>
+      <div className="container-fluid bg-light">
+        <div className="row gap-3">
+          {products?.products.map((category, i) => (
+            <div key={i} className="col-md-12 bg-white p-3 shadow">
+              <div className="container">
+                <div className="row">
+                  <div className="col-md-12">
+                    <div
+                      className={`d-flex align-items-center gap-4 flex-column flex-md-row ${
+                        i % 2 != 0 && "flex-md-row-reverse"
+                      }`}
+                    >
+                      <div className="w-50">
+                        <h2 className="fw-bold">{category.name}</h2>
+                        <p>{category.desc}</p>
+                      </div>
 
-                  <div className="w-50">
-                    <Slider {...sliderSettings}>
-                      {category.images.map((slice, i) => (
-                        <div key={i}>
-                          <div className="productCard">
-                            <Image key={i} src={slice} alt="" fill />
-                          </div>
-                        </div>
-                      ))}
-                    </Slider>
+                      <div className="w-50 mb-3">
+                        <Slider {...sliderSettings}>
+                          {category.images.map((slice, i) => (
+                            <div key={i}>
+                              <div className="productCard">
+                                <Image key={i} src={slice} alt="" fill />
+                              </div>
+                            </div>
+                          ))}
+                        </Slider>
+                      </div>
+                    </div>
                   </div>
                 </div>
               </div>
-            ))}
-          </div>
+            </div>
+          ))}
         </div>
       </div>
     </main>
